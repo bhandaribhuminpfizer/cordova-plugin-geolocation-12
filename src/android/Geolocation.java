f243d2df1b598bb5c62e9445b0683d839ea6e49d
BhandariBhumin <bhumin.bhandari@pfizer.com>
2022-08-23 15:19:25 +0530

    (android) fix: respect requested location accuracy on Android 12+ #250

--- src/android/Geolocation.java
+++ src/android/Geolocation.java
@@ -1,3 +1,5 @@
+
+
 /*
        Licensed to the Apache Software Foundation (ASF) under one
        or more contributor license agreements.  See the NOTICE file
@@ -23,7 +25,6 @@ import android.Manifest;
 import android.os.Build;
 
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PermissionHelper;
 import org.apache.cordova.PluginResult;
@@ -31,14 +32,17 @@ import org.apache.cordova.LOG;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import javax.security.auth.callback.Callback;
 
 public class Geolocation extends CordovaPlugin {
 
     String TAG = "GeolocationPlugin";
     CallbackContext context;
 
-    String [] permissions = { Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION };
+
+    String [] highAccuracyPermissions = { Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION };
+    String [] lowAccuracyPermissions = { Manifest.permission.ACCESS_COARSE_LOCATION };
+    String [] permissionsToRequest;
+    String[] permissionsToCheck;
 
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -46,14 +50,21 @@ public class Geolocation extends CordovaPlugin {
         context = callbackContext;
         if(action.equals("getPermission"))
         {
-            if(hasPermisssion())
+            boolean highAccuracy = args.getBoolean(0);
+            permissionsToCheck = highAccuracy ? highAccuracyPermissions : lowAccuracyPermissions;
+
+            // Always request both FINE & COARSE permissions on API <= 31 due to bug in WebView that manifests on these versions
+            // See https://bugs.chromium.org/p/chromium/issues/detail?id=1269362
+            permissionsToRequest = Build.VERSION.SDK_INT <= 31 ? highAccuracyPermissions : permissionsToCheck;
+
+            if(hasPermisssion(permissionsToCheck))
             {
-                PluginResult r = new PluginResult(PluginResult.Status.OK);
+                PluginResult r = new PluginResult(PluginResult.Status.OK, Build.VERSION.SDK_INT);
                 context.sendPluginResult(r);
                 return true;
             }
             else {
-                PermissionHelper.requestPermissions(this, 0, permissions);
+                PermissionHelper.requestPermissions(this, 0, permissionsToRequest);
             }
             return true;
         }
@@ -67,8 +78,10 @@ public class Geolocation extends CordovaPlugin {
         PluginResult result;
         //This is important if we're using Cordova without using Cordova, but we have the geolocation plugin installed
         if(context != null) {
-            for (int r : grantResults) {
-                if (r == PackageManager.PERMISSION_DENIED) {
+            for (int i=0; i<grantResults.length; i++) {
+                int r = grantResults[i];
+                String p = permissions[i];
+                if (r == PackageManager.PERMISSION_DENIED && arrayContains(permissionsToCheck, p)) {
                     LOG.d(TAG, "Permission Denied!");
                     result = new PluginResult(PluginResult.Status.ILLEGAL_ACCESS_EXCEPTION);
                     context.sendPluginResult(result);
@@ -81,7 +94,7 @@ public class Geolocation extends CordovaPlugin {
         }
     }
 
-    public boolean hasPermisssion() {
+    public boolean hasPermisssion(String[] permissions) {
         for(String p : permissions)
         {
             if(!PermissionHelper.hasPermission(this, p))
@@ -99,9 +112,23 @@ public class Geolocation extends CordovaPlugin {
 
     public void requestPermissions(int requestCode)
     {
-        PermissionHelper.requestPermissions(this, requestCode, permissions);
+        PermissionHelper.requestPermissions(this, requestCode, permissionsToRequest);
     }
 
+    //https://stackoverflow.com/a/12635769/777265
+    private <T> boolean arrayContains(final T[] array, final T v) {
+        if (v == null) {
+            for (final T e : array)
+                if (e == null)
+                    return true;
+        }
+        else {
+            for (final T e : array)
+                if (e == v || v.equals(e))
+                    return true;
+        }
 
+        return false;
+    }
 
 }
